4bd8b98c3dae89d1bf126ea3aa9975ec8f7a911c
Felipe Morales <felipe.morales.querol@gmail.com>
2022-03-14 23:45:17 -0300

    Fix remove code not used

--- api/app.js
+++ api/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const db = require('./config/db');
 
 const passport = require('passport');
 const session = require('express-session');
